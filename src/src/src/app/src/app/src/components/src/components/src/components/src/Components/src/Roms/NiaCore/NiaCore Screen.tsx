41dffc962c42865040076db1ef35a81e22045523
jazzu72 <lesane1972@gmail.com>
2026-03-02 16:28:07 -0500

    Create NiaCore Screen.tsx

---
+++ src/src/src/app/src/app/src/components/src/components/src/components/src/Components/src/Roms/NiaCore/NiaCore Screen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NiaCoreScreen: React.FC = () => {
+  return (
+    <main style={{ padding: "1rem" }}>
+      <h1>Nia Core</h1>
+      <p>Central intelligence of the House of Jazzu.</p>
+    </main>
+  );
+};
+
+export default NiaCoreScreen;
