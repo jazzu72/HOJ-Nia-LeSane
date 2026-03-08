5636b49739391a1655f996259a55541ae7b5e3f8
jazzu72 <lesane1972@gmail.com>
2026-03-08 12:45:23 -0400

    Create service-workers.js

---
+++ pages/public/public/service-workers.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", () => {
+  console.log("Nia LeSane service worker installed");
+});
+
+self.addEventListener("activate", () => {
+  console.log("Nia LeSane service worker activated");
+});
